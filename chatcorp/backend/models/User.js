c06084b673de6fc608831686c8a3b44aee669fe8
Pedro Arthur <147874622+Pedr04rthur@users.noreply.github.com>
2025-10-04 12:44:19 +0000

    desenvolvi a estrutura schema de um usuário

---
+++ chatcorp/backend/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Nome obrigatório!'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email obrigatório!'],
+        unique: true,
+        lowercase: true,
+        trim: true
+
+    },
+    password:{
+        type: String,
+        required: [true, 'Senha obrigatória!'],
+        minlength: 6
+    },
+    department: {
+        type: String,
+        required: [true, 'Departamento obrigatório!'],
+        trim: true
+    },
+    avatar: {
+        type: String,
+        DEFAULT: ''
+    }
+
+}, {
+    timestamps: true
+});
+
+
+// criptografando a senha antes de salvar
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next()
+});
+
+useSchema.methods.correctPassword = async function (candidatePassword, userPassword){
+    return await bcrypt.compare(candidatePassoword, userPassowrd);
+
+};
+
+module.exports = mongoose.model('User', userSchema);
